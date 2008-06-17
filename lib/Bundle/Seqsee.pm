375a77e93c535b4ac02d2608d68698be5f0c0711
Abhijit Mahabal <amahabal@cpan.org>
2008-06-17 20:42:37 +0000

    Import of AMAHABAL/Bundle-Seqsee-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Seqsee
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         AMAHABAL/Bundle-Seqsee-0.01.tar.gz
    gitpan-cpan-author:       AMAHABAL
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Seqsee.pm
@@ -0,0 +1,68 @@
+package Bundle::Seqsee;
+
+use 5.010000;
+our $VERSION = '0.01';
+
+
+# Preloaded methods go here.
+
+1;
+
+__END__
+
+
+=head1 NAME
+
+Bundle::Seqsee - Modules required by Seqsee not in core
+
+=head1 SYNOPSIS
+
+perl -MCPAN -e 'install Bundle::Seqsee'
+
+=head1 CONTENTS
+
+File::Slurp
+
+Smart::Comments
+
+Parse::RecDescent
+
+Class::Std
+
+Perl::Tidy
+
+Tk
+
+Tk::ComboEntry
+
+Tk::StatusBar
+
+Config::Std
+
+Sort::Key
+
+UNIVERSAL::require
+
+Sub::Installer
+
+Exception::Class
+
+Class::Multimethods
+
+Carp::Source
+
+Text::Diff::Parser
+
+=head1 DESCRIPTION
+
+Seqsee (http://code.google.com/p/seqsee1) is a computer program that attempts to extrapolate integer sequences. A full description is being worked on, and will soon be released. These modules are required by Seqsee.
+
+=head2 EXPORT
+
+None by default.
+
+=head1 AUTHOR
+
+Abhijit Mahabal
+
+=cut
